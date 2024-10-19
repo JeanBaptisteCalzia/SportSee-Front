c075496859b28389fb2fe649de373519806ef34f
JBC <hello@jeanbaptistecalzia.fr>
2024-10-19 23:24:26 +0200

    Add Header navigation component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+import "./nav.scss";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/" className="logo">
+            <img src={logo} alt="logo SportSee" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => {
+              return isActive ? "active" : "";
+            }}
+            to="/"
+          >
+            Accueil
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => {
+              return isActive ? "active" : "";
+            }}
+            to="/user"
+          >
+            Profil
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => {
+              return isActive ? "active" : "";
+            }}
+            to="/settings"
+          >
+            Réglage
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => {
+              return isActive ? "active" : "";
+            }}
+            to="/social"
+          >
+            Communauté
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Header;

08303b4390e417d72e762d9916c03497daeb062a
JBC <hello@jeanbaptistecalzia.fr>
2024-10-21 22:29:38 +0200

    Add days for XAxis for AverageSessions component

--- src/components/AverageSessions/index.jsx
+++ src/components/AverageSessions/index.jsx
@@ -21,6 +21,34 @@ function AverageSessions({ data }) {
     }
   };
 
+  if (data[0].sessions["0"].day == "1") {
+    data[0].sessions["0"].day = "L";
+  }
+
+  if (data[0].sessions["1"].day == "2") {
+    data[0].sessions["1"].day = "M";
+  }
+
+  if (data[0].sessions["2"].day == "3") {
+    data[0].sessions["2"].day = "M";
+  }
+
+  if (data[0].sessions["3"].day == "4") {
+    data[0].sessions["3"].day = "J";
+  }
+
+  if (data[0].sessions["4"].day == "5") {
+    data[0].sessions["4"].day = "V";
+  }
+
+  if (data[0].sessions["5"].day == "6") {
+    data[0].sessions["5"].day = "S";
+  }
+
+  if (data[0].sessions["6"].day == "7") {
+    data[0].sessions["6"].day = "D";
+  }
+
   return (
     <section className="average-sessions">
       <h2>
@@ -37,7 +65,12 @@ function AverageSessions({ data }) {
           // margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
         >
           {/* <CartesianGrid strokeDasharray="3 3" /> */}
-          <XAxis dataKey="day" stroke="#fff" />
+          <XAxis
+            axisLine={false}
+            dataKey="day"
+            stroke="#fff"
+            padding={{ left: 20, right: 20 }}
+          />
           <YAxis tick={false} hide />
           <Tooltip content={<CustomTooltip />} />
           <Line

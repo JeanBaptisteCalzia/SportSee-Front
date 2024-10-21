e8b17dae7f34fa3b01726fb134828b092e3009b4
JBC <hello@jeanbaptistecalzia.fr>
2024-10-21 21:04:15 +0200

    Replace number by kind for performance chart

--- src/components/Performance/index.jsx
+++ src/components/Performance/index.jsx
@@ -10,12 +10,41 @@ import {
 } from "recharts";
 
 function Performance({ data }) {
+  if (data[0].data["0"].kind == "1") {
+    data[0].data["0"].kind = "Cardio";
+  }
+
+  if (data[0].data["1"].kind == "2") {
+    data[0].data["1"].kind = "Énergie";
+  }
+
+  if (data[0].data["2"].kind == "3") {
+    data[0].data["2"].kind = "Endurance";
+  }
+
+  if (data[0].data["3"].kind == "4") {
+    data[0].data["3"].kind = "Force";
+  }
+
+  if (data[0].data["4"].kind == "5") {
+    data[0].data["4"].kind = "Vitesse";
+  }
+
+  if (data[0].data["5"].kind == "6") {
+    data[0].data["5"].kind = "Intensité";
+  }
+
   return (
     <section className="performance">
       <ResponsiveContainer width="100%" height={240}>
         <RadarChart cx="50%" cy="50%" data={data[0].data}>
           <PolarGrid radialLines={false} />
-          <PolarAngleAxis dataKey="kind" />
+          <PolarAngleAxis
+            dataKey="kind"
+            tick={{ fontSize: 12 }}
+            stroke="#fff"
+            fill="#fff"
+          />
           <PolarRadiusAxis tick={false} hide={true} axisLine={false} />
           <Radar
             dataKey="value"

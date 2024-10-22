72c0b91a21a0c47748475f3d9d4cd0276e91afe6
JBC <hello@jeanbaptistecalzia.fr>
2024-10-22 14:24:23 +0200

    Add style for error page

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Error() {
-  return <main>Error page</main>;
+  return (
+    <main>
+      <h1>Erreur 404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="btn">
+        Retourner sur la page d’accueil
+      </Link>
+    </main>
+  );
 }
 
 export default Error;

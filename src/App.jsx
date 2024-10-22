37a46939038d1851a8987a9279d0de4dc5f9db2c
JBC <hello@jeanbaptistecalzia.fr>
2024-10-22 14:10:11 +0200

    Add social page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Error from "./components/Error/";
 import Home from "./pages/Home/";
 import User from "./pages/User/";
 import Settings from "./pages/Settings/";
+import Social from "./pages/Social/";
 import "./App.scss";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/user/:id" element={<User />} />
           <Route path="/settings" element={<Settings />} />
+          <Route path="/social" element={<Social />} />
           <Route path="*" element={<Error />} />
         </Routes>
       </div>
---
+++ src/pages/Social/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Social() {
+  return (
+    <main>
+      <h1>Communauté 🚧</h1>
+    </main>
+  );
+}
+
+export default Social;

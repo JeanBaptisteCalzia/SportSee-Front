7f70b119896261e89c726309e32b11abbc805f29
JBC <hello@jeanbaptistecalzia.fr>
2024-10-24 22:53:03 +0200

    Reverse array for data performance

--- src/components/Performance/index.jsx
+++ src/components/Performance/index.jsx
@@ -10,34 +10,36 @@ import {
 } from "recharts";
 
 function Performance({ data }) {
+  const reversedArray = data[0].data.slice().reverse();
+
   if (data[0].data["0"].kind == "1") {
-    data[0].data["0"].kind = "Intensité";
+    data[0].data["0"].kind = "Cardio";
   }
 
   if (data[0].data["1"].kind == "2") {
-    data[0].data["1"].kind = "Vitesse";
+    data[0].data["1"].kind = "Energie";
   }
 
   if (data[0].data["2"].kind == "3") {
-    data[0].data["2"].kind = "Force";
+    data[0].data["2"].kind = "Endurance";
   }
 
   if (data[0].data["3"].kind == "4") {
-    data[0].data["3"].kind = "Endurance";
+    data[0].data["3"].kind = "Force";
   }
 
   if (data[0].data["4"].kind == "5") {
-    data[0].data["4"].kind = "Energie";
+    data[0].data["4"].kind = "Vitesse";
   }
 
   if (data[0].data["5"].kind == "6") {
-    data[0].data["5"].kind = "Cardio";
+    data[0].data["5"].kind = "Intensité";
   }
 
   return (
     <section className="performance">
       <ResponsiveContainer width="100%" height={170}>
-        <RadarChart cx="50%" cy="50%" data={data[0].data}>
+        <RadarChart cx="50%" cy="50%" data={reversedArray}>
           <PolarGrid radialLines={false} />
           <PolarAngleAxis
             dataKey="kind"

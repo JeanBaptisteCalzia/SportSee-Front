8d9b84577a65e12b9132ecd3239ee470f46711f6
JBC <hello@jeanbaptistecalzia.fr>
2024-10-25 16:59:30 +0200

    Customize activity chart

--- src/components/Activity/index.jsx
+++ src/components/Activity/index.jsx
@@ -62,9 +62,7 @@ function Activity({ data }) {
             stroke="#777"
             type="number"
             strokeWidth="1px"
-            domain={["dataMin", "dataMax + 10"]}
-            interval={1}
-            tickFormatter={() => ""}
+            domain={["dataMin - 100", "dataMax + 10"]}
           />
           <Tooltip content={<CustomTooltip />} />
 
@@ -83,47 +81,6 @@ function Activity({ data }) {
             radius={[20, 20, 0, 0]}
           />
         </BarChart>
-
-        {/* <BarChart
-          width={500}
-          height={300}
-          data={data[0].sessions}
-          margin={{
-            top: 20,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="2 2" stroke="#DEDEDE" />
-          <XAxis dataKey="day" stroke="#777" axisLine={{ stroke: "#EAF0F4" }} />
-          <YAxis yAxisId="left" axisLine={false} tick={false} />
-          <YAxis
-            axisLine={false}
-            yAxisId="right"
-            orientation="right"
-            stroke="#777"
-            type="number"
-            strokeWidth="1px"
-            domain={["dataMin - 100", "dataMax + 100"]}
-          />
-          <Tooltip content={<CustomTooltip />} />
-          {/* <Legend layout="vertical" verticalAlign="top" align="right" /> 
-          {/* <Bar
-            yAxisId="left"
-            dataKey="kilogram"
-            fill="#282D30"
-            barSize={8}
-            radius={[20, 20, 0, 0]}
-          />
-          <Bar
-            yAxisId="right"
-            dataKey="calories"
-            fill="#E60000"
-            barSize={8}
-            radius={[20, 20, 0, 0]}
-          /> 
-        </BarChart> */}
       </ResponsiveContainer>
     </section>
   );

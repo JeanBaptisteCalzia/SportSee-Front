2745aa74d4e4d1c90b0e88b239ff70b175c25234
JBC <hello@jeanbaptistecalzia.fr>
2024-10-29 09:33:54 +0100

    Add PropTypes for KeyData component

--- src/components/KeyData/index.jsx
+++ src/components/KeyData/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./keyData.scss";
+import PropTypes from "prop-types";
 
 function KeyData({ keyData, icon, title, style, unit }) {
   const options = { maximumFractionDigits: 2 };
@@ -24,5 +25,13 @@ function KeyData({ keyData, icon, title, style, unit }) {
     </>
   );
 }
+
+KeyData.propTypes = {
+  keyData: PropTypes.number.isRequired,
+  icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  style: PropTypes.string,
+  unit: PropTypes.string.isRequired,
+};
 
 export default KeyData;
